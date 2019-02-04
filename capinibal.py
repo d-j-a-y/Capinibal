d3d46211a87223ea7f10da0f72efb3f16ef52b87
vloop <marc.perilleux@laposte.net>
2019-02-04 17:59:00 +0100

    Add files via upload
    Changed command line options

--- capinibal.py
+++ capinibal.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-"""Main file for Capinibal the another anticapitalist images generator."""
+"""Main file for Capinibal another anticapitalist images generator."""
 
 from wand.image import Image
 from wand.drawing import Drawing
 from wand.color import Color
+
+# from sys import sys.stderr
+import sys
 
 import random
 import subprocess
@@ -100,7 +103,7 @@ def cpb_img_gen_matrix (cpb_textes, ctx, align_center = False):
 #    with Image(width=int(metrics.text_width)*2+15, height=int(metrics.text_height)*5, background=Color('lightblue')) as img:
     with Image(width=1024, height=576, background=Color('lightblue')) as img:
         half_width = 512;
-        print(img)
+#        print(img)
         textes_len = int(len (cpb_textes) / 2)
         with Drawing(drawing=ctx) as clone_ctx:  #<= Clones & reuse the parent context.
             for i in range (0, textes_len) :
@@ -109,13 +112,14 @@ def cpb_img_gen_matrix (cpb_textes, ctx, align_center = False):
                     clone_ctx.text(half_width - int(metrics.text_width) - 1 , (1+i)*int(metrics.ascender), cpb_textes[i])
                     clone_ctx.text(half_width + 1, (1+i)*int(metrics.ascender), cpb_textes[i+textes_len])
                 else :
-                    clone_ctx.text(5, (1+i)*int(metrics.ascender), cpb_textes[i])
+                    clone_ctx.text(25, (1+i)*int(metrics.ascender), cpb_textes[i]) # 5
                     clone_ctx.text(int(metrics.text_width), (1+i)*int(metrics.ascender), cpb_textes[i+textes_len])
                 clone_ctx(img)
 
         return img.clone()
         # ~ display(img)
 
+# currently unused
 def cpb_seq_gen_matrix (cpb_textes, ctx, pipe) :
     textes_len = len (cpb_textes)
     coord_len = int(textes_len * 4)
@@ -128,7 +132,7 @@ def cpb_seq_gen_matrix (cpb_textes, ctx, pipe) :
         quarter_width = 256
         deci_height = 57
         for i in range (0, coord_len) :
-            print(img)
+#            print(img)
             coord_index = random.randrange(0, len(candidate_coord))
             coord = candidate_coord[coord_index]
             candidate_coord.pop(coord_index)
@@ -146,7 +150,7 @@ def cpb_img_gen_solo_centered (cpb_texte, ctx):
     metrics = cpb_get_text_metrics ( cpb_texte, ctx ) # FIXME all texts are not same lenght, loop to get max widht ?
 #    with Image(width=int(metrics.text_width)*2+15, height=int(metrics.text_height)*5, background=Color('lightblue')) as img:
     with Image(width=1024, height=576, background=Color('lightgreen')) as img:
-        print(img)
+#        print(img)
         textes_len = int(len (cpb_texte) / 2)
         with Drawing(drawing=ctx) as clone_ctx:  #<= Clones & reuse the parent context.
             clone_ctx.text(int((img.width - metrics.text_width )/2), int((img.height+metrics.ascender)/2), cpb_texte)
@@ -159,7 +163,8 @@ def cpb_img_gen_solo_rdn_size_centered (cpb_texte, ctx, coin=1) :
     old_size = ctx.font_size
     if (cpb_toss (coin)) :
         ctx.font_size = int (random.randrange(55, 200, 15))
-        print ("font size " , ctx.font_size)
+        # if (args['debug']):
+        # print ("font size " , ctx.font_size)
 
     img = cpb_img_gen_solo_centered(cpb_texte, ctx)
     ctx.font_size = old_size
@@ -170,27 +175,31 @@ def cpb_img_gen_solo_rdn_size_centered (cpb_texte, ctx, coin=1) :
 # MainLoop (will be slot machine) and Main
 #
 #########################################
-def cpb_capinibal ( pipe ):
+def cpb_capinibal ( pipe, frames ):
     with Drawing() as ctx :
         ctx.font = './Sudbury_Basin_3D.ttf' #FIXME !
         ctx.font_size = 110
         ctx.fill_color = Color('black')
 
-        while False:
-            cpb_text_color_gen (ctx, 3)
-            cpb_textes = cpb_text_gen_solo()
-            blob = cpb_img_gen_solo_rdn_size_centered(cpb_textes, ctx, 5).make_blob('RGB')
-            pipe.stdin.write ( blob )
+#        while False:
+#            cpb_text_color_gen (ctx, 3)
+#            cpb_textes = cpb_text_gen_solo()
+#            blob = cpb_img_gen_solo_rdn_size_centered(cpb_textes, ctx, 5).make_blob('RGB')
+#            pipe.stdin.write ( blob )
 
 #        return
 
+        step = 1 if (frames>0) else 0 # 0 => infinite loop
         in_loop = 0 # number of matrix image
         in_blink = 5 # number of matrix(s) loop
         in_matrix = False
         matrix_align = False
         try:
-            while True:
-                cpb_text_color_gen (ctx, 3)
+            # Loop over frames
+            while (frames>=0):
+                frames -= step
+                print ("frames:", frames, " in_loop:", in_loop, " in_blink:", in_blink, " in_matrix:", in_matrix, " matrix_align:", matrix_align )
+                cpb_text_color_gen (ctx, 1) # 3
                 if (in_matrix == False):
                     cpb_textes = cpb_text_gen_solo()
                     blob = cpb_img_gen_solo_rdn_size_centered(cpb_textes, ctx, 5 ).make_blob('RGB')
@@ -201,60 +210,61 @@ def cpb_capinibal ( pipe ):
                 pipe.stdin.write( blob )
 
                 in_loop += 1
-                if (in_loop > 24): # 24 -> fps
+                if (in_loop > 24): # 1 s @ 24 fps
                     in_matrix = True if (in_matrix == False) else False
                     in_blink = in_blink - 1
                     if (in_blink > 0):
                         in_loop = 0
                     else:
                         if (in_blink < - 20):
-                            matrix_align = True if (matrix_align == False) else False
+                            # matrix_align = True if (matrix_align == False) else False
                             in_blink = 5
-
-        except SystemExit as e:
-            return
+            print("All frames generated")
+#        except KeyboardInterrupt: # Use this instead of signal.SIGINT
+#        except SystemExit as e:
+        except BrokenPipeError:
+            print("Exception caught, exiting!")
+        return
 
 
 # main
 if __name__=="__main__":
 
     parser = argparse.ArgumentParser(description='Generate another anticapitalist moving images to a named pipe...or not.')
-    parser.add_argument('-r', '--record', action='store_true',
-                       help='Record has video file')
-#    parser.add_argument('-p', '--pipe', action='store_true',
- #                      help='Name of the pipe to stream to, if missing, name is generated')
+    parser.add_argument('-o', '--output', dest='outputfile',
+                       help='Output file')
+    parser.add_argument('-r', '--rate', dest='fps', default='24',
+                       help='Frames per second')
+    parser.add_argument('-d', '--duration', dest='duration', default='0',
+                       help='Seconds, 0 for infinite')
+    parser.add_argument('-p', '--pipe', dest='pipename',
+                       help='Name of the pipe to stream to, if missing, name is generated')
     parser.add_argument('-v', '--verbose', action='store_true',
                        help='Enable debug output (default: off)')
     args = vars(parser.parse_args())
+    #print(args)
 
     random.seed()
 
-#    rate = 1
-    fps, duration = 24, 5
+    fps=args['fps']
+    duration=args['duration']
+    frames = int(float(fps) * float(duration))
+    outputfile=args['outputfile']
+    print('Generating ', frames, ' frames for ', duration, ' seconds at ', fps, ' fps.')
 
-    if (args['record'] == True):
-        cmdstring = ['ffmpeg',
-                    '-y', # (optional) overwrite output file if it exists
-                    '-f', 'rawvideo', #TODO '-f', 'image2pipe', '-vcodec', 'mjpeg' avec blob('jpeg')
-                    '-c:v','rawvideo',
-                    '-s', '1024x576', # size of one frame
-                    '-pix_fmt', 'rgb24',
-                    '-r', '8', # frames per second
-                    '-i', '-', # The imput comes from a pipe
-                    '-an', # Tells FFMPEG not to expect any audio
-                    '-c:v', 'mjpeg',
-                    '-q', '1',
-                    'capinibal.mp4' ] # Unique name
-#    else if (args['image'] == True):
-    else:
-        tmpdir = tempfile.mkdtemp()
-        filename = os.path.join(tmpdir, 'myfifo')
-        print ("The fifo is on :\n%s" % filename)
+    if (outputfile is None):
+        # No output file, use a pipe
+        if (args['pipename'] is None):
+            tmpdir = tempfile.mkdtemp()
+            filename = os.path.join(tmpdir, 'myfifo')
+        else:
+            filename=args['pipename']
+        print ("The fifo is on :\n%s" % filename, file=sys.stderr)
 
         try:
             os.mkfifo(filename)
         except OSError as e:
-            print ("Failed to create FIFO: %s" % e)
+            print ("Failed to create FIFO: %s" % e, file=sys.stderr)
             quit()
 
         fifo = open(filename, 'w')
@@ -265,30 +275,47 @@ if __name__=="__main__":
                     '-c:v','rawvideo',
                     '-s', '1024x576', # size of one frame
                     '-pix_fmt', 'rgb24',
-                    '-r', '4', # frames per second
-                    '-i', '-', # The imput comes from a pipe
+                    '-r', fps, # frames per second
+                    '-i', '-', # The input comes from a pipe
                     '-an', # Tells FFMPEG not to expect any audio
                     '-pix_fmt', 'yuv420p',
                     '-f', 'yuv4mpegpipe',
                     filename ]
+    else:
+        # Use given output file
+        print ("The output file is :\n%s" % outputfile, file=sys.stderr)
+        # No infinite duration!
+        if (frames<1):
+            print("Non-zero duration required when using -o, aborting.", file=sys.stderr)
+            sys.exit()
+        cmdstring = ['ffmpeg',
+                    '-y', # (optional) overwrite output file if it exists
+                    '-f', 'rawvideo', #TODO '-f', 'image2pipe', '-vcodec', 'mjpeg' avec blob('jpeg')
+                    '-c:v','rawvideo',
+                    '-s', '1024x576', # size of one frame
+                    '-pix_fmt', 'rgb24',
+                    '-r', fps, # frames per second
+                    '-i', '-', # The input comes from a pipe
+                    '-an', # Tells FFMPEG not to expect any audio
+                    '-c:v', 'mjpeg',
+                    '-q', '1',
+                    outputfile ] # Unique name
 
     pipe = subprocess.Popen(cmdstring, stdin=subprocess.PIPE)
 
 
     time.sleep(1)
 
-    frames = duration * fps #FIXME fps same has for ffmpeg string
-#    for i in range(frames):
-
     # Here we loop
-    cpb_capinibal ( pipe )
 
-    if (args['record'] == False):
-        if (args['debug']):
-            print("Cleaning pipe stuff...")
+    cpb_capinibal (pipe, frames)
+
+    if (args['outputfile'] is None):
+        print("Cleaning pipe stuff...", file=sys.stderr)
         fifo.close()
         os.remove(filename)
         os.rmdir(tmpdir)
+    print("Terminated normally")
 
 
 #  has clone          cpb_create_image(cpb_textes, ctx).save(filename="cani_"+str(i)+".png")
