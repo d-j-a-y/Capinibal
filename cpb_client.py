65976c68fc3ba654460f8fbc0d0615e31a5a60cf
Jerome Blanchi aka d.j.a.y <d.j.a.y@free.fr>
2019-03-21 17:09:23 +0100

    client : draft stock market driven

--- cpb_client.py
+++ cpb_client.py
@@ -4,6 +4,11 @@
 
 import liblo, sys, argparse
 
+import pandas as pd
+
+import time
+
+
 parser = argparse.ArgumentParser(description='Send an OSC message to capinibal.')
 
 parser.add_argument('--host', help='IP Address', default='127.0.0.1')
@@ -33,22 +38,45 @@ elif args.decrease is not None:
 if (args.verbose):
     print (speed, inc, dec)
 
-# send message to given port on specified host
-try:
-    target = liblo.Address(ip_address, port)
-except (liblo.AddressError, err):
-    print (str(err))
-    sys.exit()
+while (True):
 
-if speed is not None:
-    # send message "/cpb/speed" with float argument
-    liblo.send(target, "/cpb/speed", speed)
-elif inc is not None:
-    # send message "/cpb/increase" with int argument
-    liblo.send(target, "/cpb/increase", inc)
-elif dec is not None:
-    # send message "/cpb/decrease" with int argument
-    liblo.send(target, "/cpb/decrease", dec)
-else:
-    print ("Nothing happend !!!")
-    parser.print_help()
+    dataframes = pd.read_html('http://bourse.latribune.fr/indices.html')
+    df1 = dataframes[2]
+
+    total_var_french_stock = 0;
+
+    for var in df1 ['Variation %']:
+        total_var_french_stock += float(var[:-1].replace(',', '.'))
+        print ('var', var, 'total var', total_var_french_stock)
+
+
+    if (total_var_french_stock >= 0):
+        inc = int(total_var_french_stock * 10 )
+    else :
+        dec = int(-total_var_french_stock * 10 )
+
+    # send message to given port on specified host
+    try:
+        target = liblo.Address(ip_address, port)
+    except (liblo.AddressError, err):
+        print (str(err))
+        sys.exit()
+
+    # ~ if speed is not None:
+        # ~ # send message "/cpb/speed" with float argument
+        # ~ liblo.send(target, "/cpb/speed", speed)
+    # ~ el
+    if inc is not None:
+        # send message "/cpb/increase" with int argument
+        print ("Send inc", inc)
+        liblo.send(target, "/cpb/increase", inc)
+    elif dec is not None:
+        # send message "/cpb/decrease" with int argument
+        print ("Send dec", dec)
+        liblo.send(target, "/cpb/decrease", dec)
+    else:
+        print ("Nothing happend !!!")
+        parser.print_help()
+
+    # Wait for 15 seconds
+    time.sleep(15)
